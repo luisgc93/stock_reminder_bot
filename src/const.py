8c652337718e92bfc26728d1c9b41cdb698bb44c
Luis Garcia Cuesta <luisgc93@gmail.com>
2020-12-13 12:32:36 +0100

    Add constants module

---
+++ src/const.py
@@ -0,0 +1,34 @@
+TW_CHAR_LIMIT = 280
+
+SHORTENED_URL_LENGTH = 23
+
+TRUNCATED_TEXT_STRING = "(...) "
+
+MAX_TRUNCATED_CHARACTER_COUNT = (
+    TW_CHAR_LIMIT - SHORTENED_URL_LENGTH - len(TRUNCATED_TEXT_STRING)
+)
+
+
+DATE_TIME_STRINGS = ["year", "month", "week", "day", "hour", "minutes"]
+
+CASHTAG = "$"
+
+
+API_LIMIT_EXCEEDED_MESSAGE = (
+    "Our standard API call frequency is 5 calls per minute and 500 calls per day."
+)
+
+API_LIMIT_EXCEEDED_RESPONSE = (
+    "Se ha excedido el límite de búsquedas (5/minuto y 500/día). "
+    "Vuelve a probar más tarde."
+)
+
+STOCK_NOT_FOUND_MESSAGE = (
+    "Invalid API call. Please retry or visit the documentation "
+    "(https://www.alphavantage.co/documentation/) for TIME_SERIES_INTRADAY."
+)
+
+STOCK_NOT_FOUND_RESPONSE = (
+    "Sorry, I can only find stocks that are traded on the NASDAQ 😓: "
+    "https://www.nasdaq.com/market-activity/stocks/screener."
+)
