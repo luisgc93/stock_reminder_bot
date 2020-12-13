595dae29d256b10c2ca5ca91933b00218a42dffb
Luis Garcia Cuesta <luisgc93@gmail.com>
2020-12-13 16:13:38 +0100

    update method names in tests

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -63,11 +63,11 @@ class TestParseTweet:
         ],
     )
     def test_returns_stock_name_when_tweet_contains_cash_tag(self, tweet, stock_name):
-        assert bot.parse_stock_name(tweet) == stock_name
+        assert bot.parse_stock_symbol(tweet) == stock_name
 
     @pytest.mark.usefixtures("mock_alpha_vantage_get_intra_day")
     def test_returns_stock_price_with_two_decimal_places(self):
-        price = bot.get_stock_price("BABA")
+        price = bot.get_price("BABA")
 
         assert price == "$276.80"
 
