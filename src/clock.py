38640120407858656156a3eb23b3d3af4724e10a
Luis Garcia Cuesta <luisgc93@gmail.com>
2020-12-19 17:17:25 +0100

    let's run a test in production

--- src/clock.py
+++ src/clock.py
@@ -3,7 +3,7 @@ from os import environ
 import sentry_sdk
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-from . import bot
+from . import bot, const
 
 sentry_sdk.init(environ["SENTRY_PROJECT_URL"], traces_sample_rate=1.0)
 sched = BlockingScheduler()
@@ -11,6 +11,12 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job("interval", minutes=2)
 def timed_job():
+    api = bot.init_tweepy()
+    api.update_with_media(
+        filename=const.MR_SCROOGE_IMAGE_PATH,
+        status="TEST" + const.POSITIVE_RETURNS_EMOJI,
+        in_reply_to_status_id=1340323379136634880,
+    )
     bot.reply_to_mentions()
 
 
