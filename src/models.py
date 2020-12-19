65127d378a9e69023d22d1cccdb35426ee66a95f
Luis Garcia Cuesta <luisgc93@gmail.com>
2020-12-19 18:03:59 +0100

    fix migrations method

--- src/models.py
+++ src/models.py
@@ -38,7 +38,7 @@ class Reminder(BaseModel):
 
 def migrate():
     tables = db.get_tables()
-    if not tables:
+    if [Mention, Reminder] not in tables:
         db.create_tables([Mention, Reminder])
 
 
