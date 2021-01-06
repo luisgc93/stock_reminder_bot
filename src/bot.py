3db505408c955a8bcb0caaa79eb4fd7ab329d025
Luis Garcia Cuesta <luisgc93@gmail.com>
2021-01-06 01:28:48 +0100

    remove table_conversion argument

--- src/bot.py
+++ src/bot.py
@@ -207,7 +207,7 @@ def generate_report(stock):
         )
         rating_data = rating_response.json()
         data_formatted = pd.DataFrame(rating_data["ratingDetails"]).T
-        dfi.export(data_formatted, "rating_table.png", table_conversion="matplotlib")
+        dfi.export(data_formatted, "rating_table.png")
     save_report_to_image(data)
 
 
