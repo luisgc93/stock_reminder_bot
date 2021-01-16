c5238b5363cb1dd2f22d8bd1293d7e2a2859416a
Luis Garcia Cuesta <luisgc93@gmail.com>
2021-01-16 16:52:46 +0100

    use chromedriver_binary

--- src/bot.py
+++ src/bot.py
@@ -2,6 +2,7 @@ import os
 import random
 import re
 
+import chromedriver_binary
 import giphy_client
 import pytz
 
@@ -246,6 +247,7 @@ def generate_rating(stock):
         dfi.export(
             data_formatted,
             "rating_table.png",
+            chrome_path=chromedriver_binary.utils.get_chromedriver_path()
         )
     return ", ".join(ratings_list) + ". Details: "
 
